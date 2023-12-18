098d5c9edcba86e621a81e1fa57ad7a2cbe038dc
Annsrutto <annsrutto567@gmail.com>
2023-12-19 02:52:19 +0300

    Bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -8,7 +8,7 @@
 void bubble_sort(int *array, size_t size)
 {
 	size_t i, j;
-	int swapped;
+	bool swapped;
 	int temp;
 
 	if (array == NULL)
@@ -17,7 +17,7 @@ void bubble_sort(int *array, size_t size)
 	}
 	for (i = 0; i < size - 1; i++)
 	{
-		swapped = 0;
+		swapped = false;
 		for (j = 0; j < size - i - 1; j++)
 		{
 			if (array[j] > array[j + 1])
@@ -25,12 +25,12 @@ void bubble_sort(int *array, size_t size)
 				temp = array[j];
 				array[j] = array[j + 1];
 				array[j + 1] = temp;
-				swapped = 1;
+				swapped = true;
 				print_array(array, size);
 			}
 		}
 
-		if (swapped == 0)
+		if (swapped == false)
 			break;
 	}
 }
