198015c1bdbd3ab5f9eff597afa24bd93d8985b2
Annsrutto <annsrutto567@gmail.com>
2023-12-19 03:15:09 +0300

    Selection sort

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -14,6 +14,7 @@ void selection_sort(int *array, size_t size)
 	{
 		return;
 	}
+
 	for (i = 0; i < size - 1; i++)
 	{
 		min_idx = i;
@@ -23,14 +24,14 @@ void selection_sort(int *array, size_t size)
 			{
 				min_idx = j;
 			}
+		}
 
-			if (min_idx != i)
-			{
-				temp = array[i];
-				array[i] = array[min_idx];
-				array[min_idx] = temp;
-				print_array(array, size);
-			}
+		if (min_idx != i)
+		{
+			temp = array[i];
+			array[i] = array[min_idx];
+			array[min_idx] = temp;
+			print_array(array, size);
 		}
 	}
 }
