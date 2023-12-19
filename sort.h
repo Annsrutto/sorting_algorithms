7cf6a710e088ed1c2cd9018fe54560ffe8ea1de1
Annsrutto <annsrutto567@gmail.com>
2023-12-20 02:15:42 +0300

    Quick sort

--- sort.h
+++ sort.h
@@ -25,4 +25,7 @@ void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void print_list(const listint_t *list);
 void print_array(const int *array, size_t size);
+int partition(int *array, ssize_t low, ssize_t high, ssize_t size);
+void swap(int *a, int *b);
+void quicksort(int *array, ssize_t low, ssize_t high, size_t size);
 #endif /*SORT_H*/
